3144006bde830d41743440fb3616397cce977058
shvetsanna <shvetsanna139@gmail.com>
2025-11-28 21:06:31 +0200

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -6,21 +6,24 @@ int main() {
     printf("Введіть x: ");
     scanf("%d", &x);
 
-    if (x > -15)
+    if (x > -15) {
         if (x <= 3)
             y = 4*x*x + 2;
-        else
+        else {
             printf("Функція не існує\n");
-    else
+            return 0;
+        }
+    } 
+    else {
         if (x <= -30)
             y = 3*x/4 - 5;
-        else
-            if (x > 20)
-                y = 3*x/4 - 5;
-            else {
-                printf("Функція не існує\n");
-                return 0;
-            }
+        else if (x > 20)
+            y = 3*x/4 - 5;
+        else {
+            printf("Функція не існує\n");
+            return 0;
+        }
+    }
 
     printf("y = %d\n", y);
     return 0;
