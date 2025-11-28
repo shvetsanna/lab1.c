87c04f7b5d7ee4532c1419f75cb55386163cee3d
shvetsanna <shvetsanna139@gmail.com>
2025-11-28 21:09:59 +0200

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -1,3 +1,4 @@
+// Без логічних операцій
 #include <stdio.h>
 
 int main() {
@@ -23,6 +24,39 @@ int main() {
             printf("Функція не існує\n");
             return 0;
         }
+
+
+// З логічними операціями
+        
+        #include <stdio.h>
+
+int main() {
+    int x, y;
+
+    printf("Введіть x: ");
+    scanf("%d", &x);
+
+    printf("[LOG] Отримано x = %d\n", x);
+
+    if ((x > -15) && (x <= 3)) {
+        printf("[LOG] Виконується формула y = 4*x*x + 2\n");
+        y = 4*x*x + 2;
+    }
+    else if ((x <= -30) || (x > 20)) {
+        printf("[LOG] Виконується формула y = 3*x/4 - 5\n");
+        y = 3*x/4 - 5;
+    }
+    else {
+        printf("[LOG] Значення x не належить жодному інтервалу\n");
+        printf("Функція не існує\n");
+        return 0;
+    }
+
+    printf("[LOG] Результат y = %d\n", y);
+    printf("y = %d\n", y);
+
+    return 0;
+}
     }
 
     printf("y = %d\n", y);
